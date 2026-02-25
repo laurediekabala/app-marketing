e842a6a2b4f62df27d92d0e57e1d22fd946d88e2
laurenzo <laurediekabala@gmail.com>
2026-02-25 03:01:06 +0100

    modification

--- api_flask/api.py
+++ api_flask/api.py
@@ -106,4 +106,5 @@ def predict():
         logger.error(f"Traceback : {traceback.format_exc()}")
         return jsonify({'status': 'error', 'error': str(e)}), 500  
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
--- index.py
+++ index.py
@@ -230,7 +230,6 @@ def display_page(pathname, theme_value):
         ])
 
 if __name__ == '__main__':
-    #port = int(os.environ.get("PORT", 5000))
-    #app.run(host="0.0.0.0", port=port)
-     app.run(host="0.0.0.0", port=8050, debug=True)
+   port = int(os.environ.get("PORT", 5000))
+   app.run(host="0.0.0.0", port=port)
     
